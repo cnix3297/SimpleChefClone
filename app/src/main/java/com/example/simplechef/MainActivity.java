2d6cec076881a23014143a99e2c908c98314e83d
ColbySchueller <33610624+ColbySchueller@users.noreply.github.com>
2019-02-06 19:11:18 -0500

    Update me

--- app/src/main/java/com/example/simplechef/MainActivity.java
+++ app/src/main/java/com/example/simplechef/MainActivity.java
@@ -2,12 +2,18 @@ package com.example.simplechef;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button Login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Login = (Button)findViewById(R.id.LogInBtn);
+
     }
 }
