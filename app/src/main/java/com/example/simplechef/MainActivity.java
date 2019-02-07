fe416c94508920cb5dd91d6199460a25d64e6a67
ColbySchueller <33610624+ColbySchueller@users.noreply.github.com>
2019-02-06 22:20:24 -0500

    Drawable  +facebookicon.png  +googleicon.png  +login_lines.xml
    MainActivity
     +Hide signup and show login for facebook and google. Icons are added. Lines around "OR" are added to.
    
    Values
    +New strings

--- app/src/main/java/com/example/simplechef/MainActivity.java
+++ app/src/main/java/com/example/simplechef/MainActivity.java
@@ -1,18 +1,25 @@
 package com.example.simplechef;
 
 import android.media.FaceDetector;
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button Login, FacebookLogin, GoogleLogin;
     private Button Signup;
-    private ImageView FaceBookIcon, GoogleIcon;
+    private ImageView FaceBookIcon, GoogleIcon, ORLine1, ORLine2;
+    private TextView LoginPage_OR;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,17 +32,38 @@ public class MainActivity extends AppCompatActivity {
         FaceBookIcon = (ImageView)findViewById(R.id.FacebookIcon);
         GoogleLogin = (Button)findViewById(R.id.GoogleLogin);
         GoogleIcon = (ImageView)findViewById(R.id.GoogleIcon);
+        LoginPage_OR = (TextView) findViewById(R.id.OR);
+        ORLine1 = (ImageView) findViewById(R.id.ORStartLine);
+        ORLine2 = (ImageView) findViewById(R.id.OREndLine);
 
         //Login click listener
         Login.setOnClickListener(new View.OnClickListener(){
 
             @Override
             public void onClick(View v) {
+                //Animation types
+                Animation move = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.move);
+                Animation FadeHalf = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade_50);
+                Animation FadeThirds = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade_75);
+                //Fade In
+                GoogleLogin.startAnimation(move);
+                FacebookLogin.startAnimation(move);
+                LoginPage_OR.startAnimation(FadeThirds);
+                ORLine1.startAnimation(FadeHalf);
+                ORLine2.startAnimation(FadeHalf);
+                FaceBookIcon.startAnimation(move);
+                GoogleIcon.startAnimation(move);
+
+
+
                 Signup.setVisibility(View.INVISIBLE);
                 FacebookLogin.setVisibility(View.VISIBLE);
                 GoogleLogin.setVisibility(View.VISIBLE);
                 GoogleIcon.setVisibility(View.VISIBLE);
                 FaceBookIcon.setVisibility(View.VISIBLE);
+                LoginPage_OR.setVisibility(View.VISIBLE);
+                ORLine1.setVisibility(View.VISIBLE);
+                ORLine2.setVisibility(View.VISIBLE);
 
             }
         });
