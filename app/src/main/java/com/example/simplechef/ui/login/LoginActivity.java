dab0985cbbcef0f38174aa6686cc4f811bfe0cd6
Deisun <rdouma81@gmail.com>
2019-02-15 21:12:53 -0500

    Fixed bug with login background scaling.

--- app/src/main/java/com/example/simplechef/ui/login/LoginActivity.java
+++ app/src/main/java/com/example/simplechef/ui/login/LoginActivity.java
@@ -2,7 +2,6 @@ package com.example.simplechef.ui.login;
 
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.drawable.Drawable;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -109,8 +108,10 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
         // Glide handles auto-scaling images down to proper resolution
         Glide
                 .with(context)
-                .load(R.drawable.coffee3)
-                .fitCenter()
+                .load(R.drawable.login_background)
+                //.fitCenter()
+                .centerCrop()
+                .placeholder(R.drawable.login_background)
                 .into(imageViewBackground);
     }
 
