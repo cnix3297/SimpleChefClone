dece7e220b5be757a56ede323e5c6e1b8fda3bf1
Rob Douma <rdouma81@gmail.com>
2019-03-11 12:27:28 -0400

    Facebook Sign in Re-instated

--- app/src/main/java/com/example/simplechef/ui/login/LoginFragment.java
+++ app/src/main/java/com/example/simplechef/ui/login/LoginFragment.java
@@ -17,6 +17,7 @@ import android.widget.Toast;
 
 //Google SDK Imports
 import com.example.simplechef.R;
+import com.example.simplechef.ui.account.AccountActivity;
 import com.example.simplechef.util.GlideApp;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
@@ -31,6 +32,25 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
 
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookSdk;
+import com.facebook.FacebookException;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.Profile;
+import com.facebook.login.LoginBehavior;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import org.json.JSONObject;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Arrays;
+
 import static android.support.constraint.Constraints.TAG;
 
 
@@ -42,6 +62,9 @@ public class LoginFragment extends Fragment {
 
     private GoogleSignInClient mGoogleSignInClient;
     private static final int RC_SIGN_IN = 9001;
+
+    private CallbackManager callbackManager;
+
 
     public FirebaseAuth getmAuth() {
         return mAuth;
@@ -113,7 +136,7 @@ public class LoginFragment extends Fragment {
         buttonFacebookLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //signInFacebook();
+                signInFacebook();
             }
         });
 
@@ -210,4 +233,82 @@ public class LoginFragment extends Fragment {
                 });
     }
 
+    private void signInFacebook() {
+        callbackManager = CallbackManager.Factory.create();
+        LoginManager.getInstance().setLoginBehavior(LoginBehavior.WEB_ONLY);
+
+        LoginManager.getInstance().logInWithReadPermissions(getActivity(), Arrays.asList("public_profile"));
+
+
+        LoginManager.getInstance().registerCallback(callbackManager,
+                new FacebookCallback<LoginResult>() {
+                    @Override
+                    public void onSuccess(LoginResult loginResult) {
+
+                        //Graph Request
+                        GraphRequest request = GraphRequest.newMeRequest(
+                                loginResult.getAccessToken(),
+                                new GraphRequest.GraphJSONObjectCallback() {
+                                    @Override
+                                    public void onCompleted(
+                                            JSONObject object,
+                                            GraphResponse response) {
+                                        Intent myIntent = new Intent(getActivity(), AccountActivity.class);
+                                        myIntent.putExtras(getFacebookData(object));
+                                        startActivity(myIntent);
+                                        getActivity().overridePendingTransition(R.anim.slide_right, R.anim.slide_left);
+                                    }
+                                });
+                        Bundle parameters = new Bundle();
+                        parameters.putString("fields", "id,name,email,gender, birthday");
+                        request.setParameters(parameters);
+                        request.executeAsync();
+
+
+                    }
+
+                    @Override
+                    public void onCancel() {
+                        Log.d("STATE", "Facebook Login Canceled");
+
+                    }
+
+                    @Override
+                    public void onError(FacebookException exception) {
+                        // App code
+                        Log.d("STATE", exception.toString());
+
+                    }
+                });
+    }
+
+    private Bundle getFacebookData(JSONObject object) {
+        Bundle bundle = new Bundle();
+
+        try {
+            String id = object.getString("id");
+            URL profile_pic;
+            try {
+                profile_pic = new URL("https://graph.facebook.com/" + id + "/picture?type=large");
+                Log.i("profile_pic", profile_pic + "");
+                bundle.putString("profile_pic", profile_pic.toString());
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+                return null;
+            }
+
+            bundle.putString("idFacebook", id);
+            if (object.has("name"))
+                bundle.putString("name", object.getString("name"));
+            if (object.has("birthday"))
+                bundle.putString("birthday", object.getString("birthday"));
+            if (object.has("gender"))
+                bundle.putString("gender", object.getString("gender"));
+
+        } catch (Exception e) {
+            Log.d("Bundle Exception: ", "BUNDLE Exception : " + e.toString());
+        }
+
+        return bundle;
+    }
 }
