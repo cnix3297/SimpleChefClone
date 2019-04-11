c125eed2b50028146f0c899554c9746a4a8ecb43
Rob Douma <rdouma81@gmail.com>
2019-04-10 23:41:54 -0400

    Creating Recipe View structures

--- app/src/main/java/com/example/simplechef/ui/recipe_view/ViewRecipeActivity.java
+++ app/src/main/java/com/example/simplechef/ui/recipe_view/ViewRecipeActivity.java
@@ -1,28 +1,17 @@
 package com.example.simplechef.ui.recipe_view;
 
 import android.content.Context;
-import android.content.Intent;
-import android.support.annotation.NonNull;
-import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
 
 import com.example.simplechef.R;
-import com.example.simplechef.ui.account.AccountActivity;
 import com.example.simplechef.ui.home.ViewPagerAdapter;
-import com.example.simplechef.ui.recipe_create.CreateRecipeActivity;
-import com.google.firebase.auth.FirebaseAuth;
 
 public class ViewRecipeActivity extends AppCompatActivity {
     private final static String TAG = "ViewRecipeActivity";
-    private BottomNavigationView bottomNavigationView;
     private ViewPager viewPager;
     private ViewPagerAdapter viewPagerAdapter;
     private TabLayout tabLayout;
