499f65cffeeb16db8605dcf1ac7fd3440f549611
Rob Douma <rdouma81@gmail.com>
2019-04-13 14:04:55 -0400

    Trying ViewModels

---
+++ app/src/main/java/com/example/simplechef/ui/home/FavoriteRecipesViewModel.java
@@ -0,0 +1,11 @@
+package com.example.simplechef.ui.home;
+
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+import android.support.annotation.NonNull;
+
+public class FavoriteRecipesViewModel extends AndroidViewModel {
+    public FavoriteRecipesViewModel(@NonNull Application application) {
+        super(application);
+    }
+}
---
+++ app/src/main/java/com/example/simplechef/ui/home/MyRecipesViewModel.java
@@ -0,0 +1,11 @@
+package com.example.simplechef.ui.home;
+
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+import android.support.annotation.NonNull;
+
+public class MyRecipesViewModel extends AndroidViewModel {
+    public MyRecipesViewModel(@NonNull Application application) {
+        super(application);
+    }
+}
