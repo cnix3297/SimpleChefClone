fd5cfe8f3f953a292161701c4887d7e952dae2fd
ColbySchueller <33610624+ColbySchueller@users.noreply.github.com>
2019-04-21 16:45:06 -0400

    Changes

--- app/src/main/java/com/example/simplechef/ui/recipe_view/ViewPagerAdapter.java
+++ app/src/main/java/com/example/simplechef/ui/recipe_view/ViewPagerAdapter.java
@@ -1,5 +1,6 @@
 package com.example.simplechef.ui.recipe_view;
 
+import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -8,8 +9,9 @@ import android.support.v4.app.FragmentPagerAdapter;
 public class ViewPagerAdapter extends FragmentPagerAdapter {
     private String [] tabTitles = new String [] { "Description", "Ingredients", "Steps" };
 
-   public ViewPagerAdapter(FragmentManager fm) {
+    public ViewPagerAdapter(FragmentManager fm) {
        super(fm);
+
    }
     @Override
     public Fragment getItem(int position) {
