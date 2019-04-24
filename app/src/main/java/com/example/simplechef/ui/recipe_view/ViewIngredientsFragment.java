6e91cb6d038aa60639e8a4916080d51cdae535b4
Rob Douma <rdouma81@gmail.com>
2019-04-23 20:09:40 -0400

    Small bug fixes to CreateRecipe ingredients

--- app/src/main/java/com/example/simplechef/ui/recipe_view/ViewIngredientsFragment.java
+++ app/src/main/java/com/example/simplechef/ui/recipe_view/ViewIngredientsFragment.java
@@ -15,7 +15,12 @@ import com.example.simplechef.R;
 import com.google.api.LogDescriptor;
 
 public class ViewIngredientsFragment extends Fragment {
-    LinearLayout vertical;
+
+    private LinearLayout vertical;
+    private ImageView imageViewIngredientImage;
+    private TextView textViewIngredientName;
+    private TextView textViewIngredientQuantitiy;
+
     public static ViewIngredientsFragment newInstance() {
         ViewIngredientsFragment fragment = new ViewIngredientsFragment();
         return fragment;
@@ -25,37 +30,24 @@ public class ViewIngredientsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_recipe_view_ingredients, container, false);
-        vertical = (LinearLayout) view.findViewById(R.id.LinearLayoutViewRecipeVertical);
+
+        imageViewIngredientImage = view.findViewById(R.id.imageViewIngredientImage);
+        textViewIngredientName = view.findViewById(R.id.textViewIngredientName);
+        textViewIngredientQuantitiy = view.findViewById(R.id.textViewIngredientQuantity);
+
+
 
         Bundle bundle = getArguments();
         for (int i = 0; bundle.getString("IngredientsName" + i) != null; i++){
-            LinearLayout horizontal = new LinearLayout(getContext());
-            horizontal.setOrientation(LinearLayout.HORIZONTAL);
-            horizontal.setPadding(10,10,10,10);
-            TextView quantity = new TextView(getContext());
-            quantity.setPadding(10,10,10,10);
-            TextView name = new TextView(getContext());
-            name.setPadding(10,10,10,10);
-            ImageView picture = new ImageView(getContext());
-            picture.setPadding(10,10,10,10);
-            picture.setMaxWidth(20);
-            picture.setMaxHeight(20);
-            Log.d("Image", "onCreateView: " + bundle.getString("IngredientsImage" + i));
-            Log.d("Name", "onCreateView: " + bundle.getString("IngredientsName" + i));
             Glide.with(getContext())
                     .load(bundle.getString("IngredientsImage" + i))
                     .centerInside()
-                    .into(picture);
-            quantity.setText(bundle.getString("IngredientsQuantity" + i));
-            quantity.setTextSize(20);
-            name.setText(bundle.getString("IngredientsName" + i));
-            name.setTextSize(20);
-            horizontal.addView(picture);
-            horizontal.addView(name);
-            horizontal.addView(quantity);
+                    .into(imageViewIngredientImage);
+            textViewIngredientQuantitiy.setText(bundle.getString("IngredientsQuantity" + i));
+            textViewIngredientName.setText(bundle.getString("IngredientsName" + i));
 
-            vertical.addView(horizontal);
         }
+
         // Inflate the layout for this fragment
         return view;
     }
